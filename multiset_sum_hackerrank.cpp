f2f0c59990566ec037aaaa703f8e6b984be90342
Siddharth <siddharthfulia18092002@gmail.com>
2023-02-01 07:05:52 +0530

    Multimap question hackerrank,question link added

---
+++ multiset_sum_hackerrank.cpp
@@ -0,0 +1,69 @@
+//https://www.hackerearth.com/practice/data-structures/trees/heapspriority-queues/practice-problems/algorithm/monk-and-the-magical-candy-bags/
+#include <bits/stdc++.h>
+using namespace std;
+// using namespace std::chrono;
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+//if RE then remove above 3 lines
+#define FastIO ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long 
+#define ull unsigned long long 
+#define ld long double
+#define vt vector
+#define ml multiset
+#define pb push_back
+#define mp make_pair
+#define pob pop_back
+#define all(c) c.begin(),c.end()
+#define each(x,a) for(auto x:a)
+#define sz(x) (ll)(x).size()
+const ll M=(ll)(1e9)+7;
+const ll N=(ll)(1e5)+10;
+ll hash_value[N][26];
+void solve(){
+   ll n,i,k,sum=0;
+   cin>>n>>k;
+   ml<ll> m;
+   for(i=0;i<n;i++){
+     ll x;
+     cin>>x;
+     m.insert(x);
+   }
+   for(i=0;i<k;i++){
+    auto it=--m.end();//terator pointing to last element of multimap
+    ll candy=*it;
+    sum+=candy;
+    m.erase(it);//use iterator to erase as it removes only 1 copy,if value is passed then all copies are deleted
+    m.insert(candy/2);
+   }
+   cout<<sum;
+   cout<<"\n";
+}
+int main(){
+    // auto start = high_resolution_clock::now();
+    // cout<<fixed<<setprecision(2);
+    #ifndef ONLINE_JUDGE
+    freopen("ip.txt","r",stdin);
+    freopen("op.txt","w",stdout);
+    #endif
+    FastIO;
+    ll t;
+    // ll i=1;
+    cin>>t;
+    while(t--){
+    // cout<<"Case #"<<i<<": ";
+    solve();
+    // cout<<"\n";
+    // i++;
+    }
+    //  auto stop = high_resolution_clock::now();
+    //   auto duration = duration_cast<microseconds>(stop - start);cout << "Time taken by function: "
+    //      << duration.count() << " microseconds" << endl;
+    return 0;
+}
+/*INPUT
+1
+5 3
+2 1 7 4 2
+*/
